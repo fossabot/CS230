7a0d7ed0edfb1c0c4258a8389b06369c384085fb
Vickram Gidwani <vickgidwani@gmail.com>
2018-05-17 19:19:24 -0700

    updated baseline_model.py; bugs still exist...

--- baseline.py
+++ baseline.py
@@ -2,14 +2,9 @@ import numpy as np
 import tensorflow as tf
 import math
 from tensorflow.python.framework import ops
-# from cnn_utils import *
-
-np.random.seed(1)
 
 def load_dataset():
-    print("here1")
     X_train = np.load('Xtrain.npy')
-    print("here2")
     Y_train = np.load('Ytrain.npy')
     #Seq_train = np.load('Seqtrain.npy')
     X_test = np.load('Xtest.npy')
@@ -47,13 +42,8 @@ def initialize_parameters():
 
     tf.set_random_seed(1)                              # so that your "random" numbers match ours
 
-    ### START CODE HERE ### (approx. 2 lines of code)
     W1 = tf.get_variable("W1", [16,100,1,1], initializer =tf.contrib.layers.xavier_initializer(seed = 0) )
-    #W2 =  tf.get_variable("W2", [2,2,8,16], initializer =tf.contrib.layers.xavier_initializer(seed = 0))
-    ### END CODE HERE ###
-
     parameters = {"W1": W1}
-
     return parameters
 
 def forward_propagation(X, parameters):
@@ -72,31 +62,19 @@ def forward_propagation(X, parameters):
 
     # Retrieve the parameters from the dictionary "parameters"
     W1 = parameters['W1']
-    #W2 = parameters['W2']
 
-    ### START CODE HERE ###
-    # CONV2D: stride of 1, padding 'SAME'
+    # CONV2D: stride of 10 in one dimension, padding 'valid'
     Z1 = tf.nn.conv2d(X,W1, strides = [1,10,1,1], padding = 'VALID')
     # RELU
     A1 = tf.nn.relu(Z1)
-    # MAXPOOL: window 8x8, sride 8, padding 'SAME'
-    #P1 = tf.nn.max_pool(A1, ksize = [1,8,8,1], strides = [1,8,8,1], padding = 'SAME')
-    # CONV2D: filters W2, stride 1, padding 'SAME'
-    #Z2 = tf.nn.conv2d(P1,W2, strides = [1,1,1,1], padding = 'SAME')
-    # RELU
-    #A2 = tf.nn.relu(Z2)
-    # MAXPOOL: window 4x4, stride 4, padding 'SAME'
-    #P2 = tf.nn.max_pool(A2, ksize = [1,4,4,1], strides = [1,4,4,1], padding = 'SAME')
     # FLATTEN
     A1 = tf.contrib.layers.flatten(A1)
     #A1 = tf.concat([Z1,tf.transpose(Seq)],0)
-    # FULLY-CONNECTED without non-linear activation function (not not call softmax).
+    # FULLY-CONNECTED layers
     # 6 neurons in output layer. Hint: one of the arguments should be "activation_fn=None"
-    Z2 = tf.contrib.layers.fully_connected(A1, 100)
-    Z3 = tf.contrib.layers.fully_connected(Z2, 20)
-    Z4 = tf.contrib.layers.fully_connected(Z3, 1, activation_fn=None)
-
-    ### END CODE HERE ###
+    A2 = tf.contrib.layers.fully_connected(A1, 100)
+    A3 = tf.contrib.layers.fully_connected(A2, 20)
+    Z4 = tf.contrib.layers.fully_connected(A3, 1, activation_fn=None)
 
     return Z4
 
@@ -112,29 +90,27 @@ def compute_cost(Z4, Y):
     cost - Tensor of the cost function
     """
 
-    ### START CODE HERE ### (1 line of code)
     cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = Z4, labels = Y))
-    ### END CODE HERE ###
-
     return cost
+
 def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     """
     Creates a list of random minibatches from (X, Y)
-    
+
     Arguments:
     X -- input data, of shape (input size, number of examples)
     Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
     mini_batch_size -- size of the mini-batches, integer
-    
+
     Returns:
     mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
     """
-    
+
     np.random.seed(seed)            # To make your "random" minibatches the same as ours
     m = X.shape[0]
                      # number of training examples
     mini_batches = []
-        
+
     # Step 1: Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
     shuffled_X = X[permutation,:]
@@ -143,25 +119,21 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
     num_complete_minibatches = int(math.floor(m/mini_batch_size)) # number of mini batches of size mini_batch_size in your partitionning
     for k in range(0, num_complete_minibatches):
-        ### START CODE HERE ### (approx. 2 lines)
         mini_batch_X = shuffled_X[ k*mini_batch_size:(k+1)*mini_batch_size,:]
         mini_batch_Y = shuffled_Y[ k*mini_batch_size:(k+1)*mini_batch_size,:]
-        ### END CODE HERE ###
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     # Handling the end case (last mini-batch < mini_batch_size)
     if m % mini_batch_size != 0:
-        ### START CODE HERE ### (approx. 2 lines)
         mini_batch_X = shuffled_X[num_complete_minibatches*mini_batch_size:m,:]
         mini_batch_Y =shuffled_Y[num_complete_minibatches*mini_batch_size:m,:]
-        ### END CODE HERE ###
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     return mini_batches
 def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
-          num_epochs = 20, minibatch_size = 64, print_cost = True):
+          num_epochs = 1, minibatch_size = 64, print_cost = True):
     """
     Implements a three-layer ConvNet in Tensorflow:
     CONV2D ->  FLATTEN -> add SEQ data -> 3x FULLYCONNECTED
@@ -188,39 +160,28 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
     (m, n_H0, n_W0) = X_train.shape
     X_train = X_train.reshape(m,n_H0,n_W0,1)
     n_y = Y_train.shape[1]
+
+    (m1, _, _) = X_test.shape
+    X_test = X_test.reshape(m1, n_H0, n_W0, 1)
     costs = []                                        # To keep track of the cost
 
     # Create Placeholders of the correct shape
-    ### START CODE HERE ### (1 line)
-    X, Y = create_placeholders(n_H0, n_W0, n_y,1)
-    ### END CODE HERE ###
+    X, Y = create_placeholders(n_H0, n_W0, n_y, 1)
 
     # Initialize parameters
-    ### START CODE HERE ### (1 line)
     parameters = initialize_parameters()
-    ### END CODE HERE ###
-
-    print("here3")
 
     # Forward propagation: Build the forward propagation in the tensorflow graph
-    ### START CODE HERE ### (1 line)
     Z4 = forward_propagation(X, parameters)
-    ### END CODE HERE ###
 
     # Cost function: Add cost function to tensorflow graph
-    ### START CODE HERE ### (1 line)
     cost = compute_cost(Z4,Y)
-    ### END CODE HERE ###
 
     # Backpropagation: Define the tensorflow optimizer. Use an AdamOptimizer that minimizes the cost.
-    ### START CODE HERE ### (1 line)
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate).minimize(cost)
-    ### END CODE HERE ###
 
     # Initialize all the variables globally
     init = tf.global_variables_initializer()
-
-    print("here4")
 
     # Start the session to compute the tensorflow graph
     with tf.Session() as sess:
@@ -242,12 +203,8 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
                 (minibatch_X, minibatch_Y) = minibatch
                 # IMPORTANT: The line that runs the graph on a minibatch.
                 # Run the session to execute the optimizer and the cost, the feedict should contain a minibatch for (X,Y).
-                ### START CODE HERE ### (1 line)
                 _ , temp_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
-                ### END CODE HERE ###
-
                 minibatch_cost += temp_cost / num_minibatches
-
 
             # Print the cost every epoch
             if print_cost == True and epoch % 1 == 0:
@@ -255,28 +212,26 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.009,
             if print_cost == True and epoch % 1 == 0:
                 costs.append(minibatch_cost)
 
-
-        # plot the cost
-        # plt.plot(np.squeeze(costs))
-        # plt.ylabel('cost')
-        # plt.xlabel('iterations (per tens)')
-        # plt.title("Learning rate =" + str(learning_rate))
-        # plt.show()
-
         # Calculate the correct predictions
-        if tf.greater(Z4,tf.variable(0.5)):
-        	predict_op = 1
-        else:
-        	predict_op = 0
-        correct_prediction = tf.equal(predict_op, Y)
+        # def fn_true(): return tf.constant(1, dtype = tf.float32)
+        # def fn_false(): return tf.constant(0, dtype = tf.float32)
+        # predict_op = tf.cond(tf.greater(tf.squeeze(Z4), tf.constant(0.5)), true_fn = fn_true, false_fn = fn_false)
+        # Z4 = tf.squeeze(Z4)
+        # comp = tf.Variable(tf.fill(dims=tf.shape(Z4), 0.5), validate_shape=False)
+        # predict_op = tf.greater(Z4, comp)
+        # correct_prediction = tf.equal(tf.cast(predict_op, dtype = tf.float32), Y)
+        correct_prediction = tf.round(tf.squeeze(Z4))
 
         # Calculate accuracy on the test set
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-        print(accuracy)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print("here1")
         train_accuracy = accuracy.eval({X: X_train, Y: Y_train})
+        print("here2")
         test_accuracy = accuracy.eval({X: X_test, Y: Y_test})
+        print("here3")
         print("Train Accuracy:", train_accuracy)
         print("Test Accuracy:", test_accuracy)
+        print("Parameters:", parameters)
 
         return train_accuracy, test_accuracy, parameters
 
